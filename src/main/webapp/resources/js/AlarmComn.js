cea06f4805533c1818a09bc27679995ba1c8c1af
InhongLee <amaco78@gmail.com>
2018-06-02 21:05:26 +0900

    gfn_getTime
    입력받은 변수가 시간형식이면 input tag time형식에 맞춰 type변환하여 반환하고 시간형식이 아니면 "00:00:00"을
    반환

--- src/main/webapp/resources/js/AlarmComn.js
+++ src/main/webapp/resources/js/AlarmComn.js
@@ -12,3 +12,23 @@ function gfn_diffDate(stdt, eddt) {
 	var nDiffDate = nDiff/(1000*60*60*24);
 	return nDiffDate;
 }
+
+/**
+ * 입력받은 변수가 시간형식이면 input tag time형식에 맞춰 type변환하여 반환하고 시간형식이 아니면 "00:00:00"을 반환
+ * @param time
+ * @returns
+ */
+function gfn_getTime(time) {
+	var isTime = true;
+	var rslt = "00:00:00";
+	var sTime = time.replace(/[^0-9]/g,"");
+	if(sTime.length != 6) isTime = false;
+	var nHours 	= sTime.substr(0,2);
+	var nMinute = sTime.substr(2,2);
+	var nSecond = sTime.substr(4,2);
+	if(nHours  < 0 || nHours  > 23) isTime  = false;
+	if(nMinute < 0 || nMinute > 59) isTime  = false;
+	if(nSecond < 0 || nSecond > 59) isTiems = false;
+	if(isTime == true) rslt = nHours+":"+nMinute+":"+nSecond;
+	return rslt;
+}
